6dae85d7b335a7661743cf0d1fad0b9b3c80b97f
Muhammad Rizki <muhammad.rizki88@telkom.co.id>
2024-01-22 11:02:39 +0700

    add session feature

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,18 +2,57 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"time"
 
+	"github.com/alexedwards/scs/v2"
+	"github.com/r1zq/goweb/pkg/config"
 	"github.com/r1zq/goweb/pkg/handlers"
+	"github.com/r1zq/goweb/pkg/render"
 )
 
 const portNumber = ":8080"
 
+var app config.AppConfig
+var session *scs.SessionManager
+
 // main is the main function
 func main() {
-	http.HandleFunc("/", handlers.Home)
-	http.HandleFunc("/about", handlers.About)
+	// change this to true when in production
+	app.InProduction = false
 
-	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
-	_ = http.ListenAndServe(portNumber, nil)
+	// set up the session
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	fmt.Printf("Starting application on port %s\n", portNumber)
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/r1zq/goweb
 
 go 1.15
+
+require (
+	github.com/alexedwards/scs/v2 v2.7.0
+	github.com/go-chi/chi v1.5.5
+	github.com/justinas/nosurf v1.1.1 // indirect
+)
